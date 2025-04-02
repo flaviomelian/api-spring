a2c092eb2fcb579fcdffb0625e52618075bb2f89
flaviomelian <flaviomelian2000@gmail.com>
2025-04-02 15:59:13 +0100

    frontend encaminado

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -16,38 +16,46 @@ const Dashboard = () => {
     }
 
     return (
-        <div>
+        <div className='dashboard'>
             {console.log(users)}
             <h1>Equipo</h1>
             <table>
                 <thead>
                     <tr>
-                        <th>ID</th>
-                        <th>Usuario</th>
-                        <th>Nombre</th>
-                        <th>Apellidos</th>
-                        <th>Rol</th>
-                        <th>Salario</th>
-                        <th>Operaciones</th>
+                        <th className='spaced'>ID</th>
+                        <th className='spaced'>Usuario</th>
+                        <th className='spaced'>Nombre</th>
+                        <th className='spaced'>Apellidos</th>
+                        <th className='spaced'>Rol</th>
+                        <th className='spaced'>Salario</th>
+                        <th className='operations'>Operaciones</th>
                     </tr>
                 </thead>
                 <tbody>
                 {users.map((user, index) => (
                     <tr>
-                        <td key={index}>{user.id}</td>
-                        <td key={index}>{user.username}</td>
-                        <td key={index}>{user.name}</td>
-                        <td key={index}>{user.surnames}</td>
-                        <td key={index}>{user.role}</td>
-                        <td key={index}>{user.salary} €</td>
-                        <td>
+                        <td className='spaced' key={index}>{user.id}</td>
+                        <td className='spaced' key={index}>{user.username}</td>
+                        <td className='spaced' key={index}>{user.name}</td>
+                        <td className='spaced' key={index}>{user.surnames}</td>
+                        <td className='spaced' key={index}>{user.role}</td>
+                        <td className='spaced' key={index}>{user.salary} €</td>
+                        <td className='spaced'>
                             <button className="btn warning">Editar</button>
+                        </td>
+                        <td className='spaced'>
                             <button className="btn danger">Eliminar</button>
                         </td>
                     </tr>
                 ))}    
                 </tbody>
-            
+                <tfoot>
+                    <tr>
+                        <td colSpan={12} className="text-center">
+                            <button className='btn primary clean'>Crear desarrollador</button>
+                        </td>
+                    </tr>
+                </tfoot>
             </table>
         </div>
     )
