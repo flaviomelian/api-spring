396b480d468dc6cfb0f90c7c024530d0330ad0c7
flaviomelian <flaviomelian2000@gmail.com>
2025-04-02 09:03:27 +0100

    api basica en funcionamiento

--- api/src/main/java/com/flavio/api/controllers/UserController.java
+++ api/src/main/java/com/flavio/api/controllers/UserController.java
@@ -1,21 +1,13 @@
 package com.flavio.api.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import com.flavio.api.models.User;
 import com.flavio.api.services.UserService;
-
+import com.flavio.api.repositories.UserRepository;
 import java.util.List;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-
+import java.util.Optional;
 
 
 @RestController
@@ -23,23 +15,42 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class UserController {
     
     private UserService userService;
+    private UserRepository userRepository;
 
     public UserController(UserService userService){
         this.userService = userService;
     }
     
     @GetMapping("/")
-    public List<User> getAllUsers(@RequestParam String param) {
+    public List<User> getAllUsers() {
         return this.userService.getAllUsers();
     }
 
+    @GetMapping("/{id}")
+    public Optional<User> getUserById(@PathVariable long id) {
+        return this.userService.getUserById(id);
+    }
+
     @PostMapping("/")
-    public User postMethodName(@RequestBody User user) {
+    public User createUser(@RequestBody User user) {
         return this.userService.saveUser(user);
     }
     
-    @PutMapping("path/{id}")
-    public void putMethodName(@PathVariable String id, @RequestBody String entity) {
+    @PutMapping("/{id}")
+    public void updateUser(@PathVariable String id, @RequestBody String entity) {
        this.userService.getUserById(null);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+        Optional<User> user = userRepository.findById(id);
+
+        if (user.isPresent()) {
+            userService.deleteUser(user.get().getId());
+            return ResponseEntity.ok("Usuario eliminado correctamente.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario no encontrado.");
+        }
+    }
+
 }
--- api/src/main/java/com/flavio/api/models/User.java
+++ api/src/main/java/com/flavio/api/models/User.java
@@ -4,10 +4,43 @@ import jakarta.persistence.*;
 
 @Entity
 @Table(name = "users")
-public record User(
+public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id,
-    String name,
-    String email
-) {}
+    private Long id;
+    private String name;
+    private String surnames;
+    private double salary;
+    private String email;
+
+    public User() {}
+
+    public User(String name, String surnames, double salary, String email) {
+        this.name = name;
+        this.surnames = surnames;
+        this.salary = salary;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurnames() {
+        return surnames;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
+
