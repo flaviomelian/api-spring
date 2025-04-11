9fc53248714f7b7d9e6d2fc767ec5f82f76871e1
flaviomelian <flaviomelian2000@gmail.com>
2025-04-11 13:07:03 +0100

    barra de scroll en el Kanban

--- frontend/src/Components/TaskCard/TaskCard.jsx
+++ frontend/src/Components/TaskCard/TaskCard.jsx
@@ -1,47 +1,13 @@
-import React, { useRef, useEffect } from 'react';
-import { useDrag, useDragLayer } from 'react-dnd';
+import React from 'react';
 import './TaskCard.css';  // Asegúrate de tener estilos para el TaskCard
 import { Link } from 'react-router-dom';
 import pen from '../../assets/pen.png';  // Asegúrate de tener la imagen en la ruta correcta
 
 const ItemType = 'TASK';
 
-const TaskCard = ({ task, onRequestDelete }) => {
-
-    const ref = useRef(null); // Para hacer referencia al componente
-
-    // Configuración de `useDrag` para el drag and drop
-    const [{ isDragging }, drag, preview] = useDrag(() => ({
-        type: ItemType,
-        item: { id: task.id },
-        collect: (monitor) => ({
-            isDragging: monitor.isDragging(),
-        }),
-    }));
-
-    // Asegúrate de que el dragPreview esté configurado para mostrar el elemento tal cual
-    useEffect(() => {
-        if (preview && ref.current) {
-            preview(ref.current);  // Esto establece el preview como el elemento mismo
-        }
-    }, [preview]);
-
-    // Estilo del elemento mientras se está arrastrando
-    const dragStyle = {
-        opacity: 1,  // Garantizamos que se mantenga opaco
-        cursor: 'move',  // Cambiar el cursor cuando el usuario esté arrastrando
-        position: isDragging ? 'absolute' : 'relative', // Aseguramos la posición al arrastrar
-    };
-
+const TaskCard = ({ task, onRequestDelete, onMoveLeft, onMoveRight }) => {
     return (
-        <div
-            ref={(node) => {
-                drag(node); // Esto es necesario para el drag
-                ref.current = node; // Referencia para el preview
-            }}
-            style={dragStyle}
-            className='task-card'
-        >
+        <div className='task-card'>
             <div className='task-card-header'>
                 <h2>{task.content}</h2>
                 <div className='task-card-labels'>
@@ -52,7 +18,9 @@ const TaskCard = ({ task, onRequestDelete }) => {
             </div>
             <div className='task-card-footer'>
                 <button className='kanban-button delete' onClick={onRequestDelete}>❌</button>
-                <Link to='/create-task' state={{ task: task, project: task.project }}><img className='pen' src={pen}/></Link>
+                <Link to='/create-task' state={{ task: task, project: task.project }}><img className='pen' src={pen} alt="Editar" /></Link>
+                {onMoveLeft && <button onClick={onMoveLeft}>⬅️</button>}
+                {onMoveRight && <button onClick={onMoveRight}>➡️</button>}
             </div>
         </div>
     );
--- frontend/src/Pages/Kanban/Kanban.jsx
+++ frontend/src/Pages/Kanban/Kanban.jsx
@@ -3,14 +3,14 @@ import { useLocation, useNavigate } from "react-router-dom";
 import './Kanban.css';
 import { deleteTask, getAllTasksByProject } from '../../services/services';
 import TaskCard from '../../Components/TaskCard/TaskCard';  // Importamos el componente de TaskCard
-import { useDrop } from 'react-dnd';
 
 const Kanban = () => {
     const location = useLocation();
     const project = location.state?.project;
-    const [tasks, setTasks] = useState([]);
-    const [progressTasks, setProgressTasks] = useState([]);
-    const [doneTasks, setDoneTasks] = useState([]);
+    const [tasks, setTasks] = useState([]);               
+    const [progressTasks, setProgressTasks] = useState([]); 
+    const [revisionTasks, setRevisionTasks] = useState([]); 
+    const [doneTasks, setDoneTasks] = useState([]);        
     const [taskToDelete, setTaskToDelete] = useState(null);
     const navigate = useNavigate();
 
@@ -35,58 +35,111 @@ const Kanban = () => {
         fetchTasks();
     }, [project]);
 
-    // Funciones para manejar el drop
-    const handleDrop = (taskId, column) => {
-        const task = tasks.find((task) => task.id === taskId);
-        if (column === 'En progreso') {
-            setProgressTasks([...progressTasks, task]);
-            setTasks(tasks.filter(t => t.id !== taskId));
-        } else if (column === 'Hecho') {
-            setDoneTasks([...doneTasks, task]);
-            setProgressTasks(progressTasks.filter(t => t.id !== taskId));
+    const moveTask = async (task, direction) => {
+    
+        // Clonamos la tarea para no modificar el objeto directamente
+        let updatedTask = { ...task };
+    
+        // Mover hacia la izquierda
+        if (direction === 'left') {
+            if (task.status === 'progress') {
+                updatedTask.status = 'tasks';
+                setProgressTasks((prev) => prev.filter(t => t.id !== task.id));  // Eliminar de 'progress'
+                setTasks((prev) => [...prev, updatedTask]);  // Agregar a 'tasks'
+            } else if (task.status === 'in_revission') {
+                updatedTask.status = 'progress';
+                setRevisionTasks((prev) => prev.filter(t => t.id !== task.id));  // Eliminar de 'in_revission'
+                setProgressTasks((prev) => [...prev, updatedTask]);  // Agregar a 'progress'
+            } else if (task.status === 'done') {
+                updatedTask.status = 'in_revission';
+                setDoneTasks((prev) => prev.filter(t => t.id !== task.id));  // Eliminar de 'done'
+                setRevisionTasks((prev) => [...prev, updatedTask]);  // Agregar a 'in_revission'
+            }
         }
-    };
-
-    const [, dropToDo] = useDrop(() => ({
-        accept: 'TASK',
-        drop: (item) => handleDrop(item.id, 'En progreso'),
-    }));
-
-    const [, dropInProgress] = useDrop(() => ({
-        accept: 'TASK',
-        drop: (item) => handleDrop(item.id, 'Hecho'),
-    }));
+        // Mover hacia la derecha
+        else if (direction === 'right') {
+            if (task.status === 'tasks') {
+                updatedTask.status = 'progress';
+                setTasks((prev) => prev.filter(t => t.id !== task.id));  // Eliminar de 'tasks'
+                setProgressTasks((prev) => [...prev, updatedTask]);  // Agregar a 'progress'
+            } else if (task.status === 'progress') {
+                updatedTask.status = 'in_revission';
+                setProgressTasks((prev) => prev.filter(t => t.id !== task.id));  // Eliminar de 'progress'
+                setRevisionTasks((prev) => [...prev, updatedTask]);  // Agregar a 'in_revission'
+            } else if (task.status === 'in_revission') {
+                updatedTask.status = 'done';
+                setRevisionTasks((prev) => prev.filter(t => t.id !== task.id));  // Eliminar de 'in_revission'
+                setDoneTasks((prev) => [...prev, updatedTask]);  // Agregar a 'done'
+            }
+        }
+    };    
 
     return (
         <div className='kanban'>
             <div className='header-kanban'>
-                <div ref={dropToDo} className='to-do kanban-section'>
-                    <div className='header-buttton'>
-                        <h1 className='kanban-section-header'>Por hacer</h1>
-                        <button className='kanban-button' onClick={() => navigate("/create-task", { state: { project } })}> + </button>
-                    </div>
+            <div className='to-do kanban-section'>
+                <div className='header-buttton'>
+                    <h1 className='kanban-section-header'>Por hacer</h1>
+                    <button className='kanban-button' onClick={() => navigate("/create-task", { state: { project } })}> + </button>
+                </div>
+                <div className='tasks'>
                     {tasks.map((task) => (
-                        <TaskCard key={task.id} task={task} onRequestDelete={() => setTaskToDelete(task)} />
+                        <TaskCard 
+                            key={task.id} 
+                            task={task} 
+                            onRequestDelete={() => setTaskToDelete(task)} 
+                            onMoveLeft={() => moveTask(task, 'left')} 
+                            onMoveRight={() => moveTask(task, 'right')} 
+                        />
                     ))}
                 </div>
+                
+            </div>
 
-                <div ref={dropInProgress} className='progress kanban-section'>
-                    <h1 className='kanban-section-header'>En progreso</h1>
+            <div className='progress kanban-section'>
+                <h1 className='kanban-section-header'>En progreso</h1>
+                <div className='tasks'></div>
                     {progressTasks.map((task) => (
-                        <TaskCard key={task.id} task={task} onRequestDelete={() => setTaskToDelete(task)} />
+                        <TaskCard 
+                            key={task.id} 
+                            task={task} 
+                            onRequestDelete={() => setTaskToDelete(task)} 
+                            onMoveLeft={() => moveTask(task, 'left')} 
+                            onMoveRight={() => moveTask(task, 'right')} 
+                        />
                     ))}
-                </div>
+            </div>
 
-                <div className='review kanban-section'>
-                    <h1 className='kanban-section-header'>En revisión</h1>
-                </div>
+            <div className='review kanban-section'>
+                <h1 className='kanban-section-header'>En revisión</h1>
+                <div className='tasks'></div>
+                    {revisionTasks.map((task) => (
+                        <TaskCard 
+                            key={task.id} 
+                            task={task} 
+                            onRequestDelete={() => setTaskToDelete(task)} 
+                            onMoveLeft={() => moveTask(task, 'left')} 
+                            onMoveRight={() => moveTask(task, 'right')} 
+                        />
+                    ))}
+            </div>
 
-                <div className='done kanban-section'>
-                    <h1 className='kanban-section-header'>Hecho</h1>
+            <div className='done kanban-section'>
+                <h1 className='kanban-section-header'>Hecho</h1>
+                <div className='tasks'>
                     {doneTasks.map((task) => (
-                        <TaskCard key={task.id} task={task} onRequestDelete={() => setTaskToDelete(task)} />
+                        <TaskCard 
+                            key={task.id} 
+                            task={task} 
+                            onRequestDelete={() => setTaskToDelete(task)} 
+                            onMoveLeft={() => moveTask(task, 'left')} 
+                            onMoveRight={() => moveTask(task, 'right')}
+                        />
                     ))}
                 </div>
+                
+            </div>
+
             </div>
             {taskToDelete && (
             <div className="modal-backdrop">
